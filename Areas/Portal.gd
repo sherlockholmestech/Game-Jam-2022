90ded1cb0aa8160d2006a54e5b6d2e1a840044d7
Sherlock Holmes <sherlockholmesxp@gmail.com>
2022-06-14 16:27:04 +0800

    Added timer

--- Areas/Portal.gd
+++ Areas/Portal.gd
@@ -5,14 +5,18 @@ signal portal_entered
 
 export(String, FILE, "*.tscn") var target_scene
 
+func _ready() -> void:
+	PlayerData.levelup = false
 
 func next_level():
+	PlayerData.levelup = true
 	get_tree().change_scene(target_scene)
 
 
 func _on_Portal_body_entered(body: Node) -> void:
 	if body.is_in_group("Player"):
 		emit_signal("portal_entered")
+		PlayerData.level = PlayerData.level + 1
 		yield(get_tree().create_timer(1.2), "timeout")
 		next_level()
 	else:
--- Player/PlayerData.gd
+++ Player/PlayerData.gd
@@ -1,14 +1,51 @@
 extends Node
 
+var timer_on = true
 var score = 0
 var isonfire = false
 var isfrozen = false
+var time_elapsed = 0.0
+var totaltime = 0
+var formattedtime
+var levelup = false
+var level = 0 setget level_calc
+var died = 0 setget die_calc
 
-
+func _ready() -> void:
+	timer_on = true
+	$CanvasLayer/Time.clear()
+	
 func _process(delta: float) -> void:
+	if levelup == false:
+		formattedtime = _format_seconds(time_elapsed, false)
+	if timer_on:
+		time_elapsed += delta
 	if score > 0:
-		$CanvasLayer/Label.visible = true
-		$CanvasLayer/Label.text = "Score: %s" % score
+		$CanvasLayer/Score.visible = true
+		$CanvasLayer/Score.text = "Score: %s" % score
 	else:
-		$CanvasLayer/Label.visible = false
+		$CanvasLayer/Score.visible = false
+	if time_elapsed > 0 and levelup == false and timer_on:
+		$CanvasLayer/Time.clear()
+		$CanvasLayer/Time.visible = true
+		$CanvasLayer/Time.add_text(formattedtime)
+	else:
+		$CanvasLayer/Time.clear()
 
+func _format_seconds(time : float, use_milliseconds : bool) -> String:
+	var minutes := time / 60
+	var seconds := fmod(time, 60)
+	if not use_milliseconds:
+		return "%02d:%02d" % [minutes, seconds]
+	var milliseconds := fmod(time, 1) * 100
+	return "%02d:%02d:%02d" % [minutes, seconds, milliseconds]
+
+func level_calc(level):
+	timer_on = false
+	totaltime += time_elapsed
+	$CanvasLayer/Time.clear()
+	$CanvasLayer/Time.visible = false
+	print(totaltime)
+
+func die_calc(level):
+	pass
